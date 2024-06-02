55851a0431e58873af7f43db83c72c781b658ec7
syahn <496300@naver.com>
2024-06-02 20:55:03 +0900

    init : 초기 프로젝트 세팅

---
+++ src/main/java/com/example/luckyvicky/LuckyVickyApplication.java
@@ -0,0 +1,13 @@
+package com.example.luckyvicky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuckyVickyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuckyVickyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/luckyvicky/LuckyVickyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.luckyvicky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuckyVickyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
