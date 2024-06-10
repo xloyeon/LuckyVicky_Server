15e3ab6f8742e960e07b0611196c277afe3e8091
syahn <496300@naver.com>
2024-06-10 15:31:43 +0900

    refactor: SecurityConfig 설정 수정

--- src/main/java/com/example/luckyvicky/common/config/SecurityConfig.java
+++ src/main/java/com/example/luckyvicky/common/config/SecurityConfig.java
@@ -1,21 +1,38 @@
 package com.example.luckyvicky.common.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
+@Configuration
 public class SecurityConfig {
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity)throws Exception {
         httpSecurity
-                .authorizeRequests()
-                .anyRequest().permitAll()
-                .and().formLogin((f)-> f.disable());
+                .authorizeHttpRequests((authorizeRequests) ->
+                        authorizeRequests
+                                .requestMatchers(
+                                        new AntPathRequestMatcher("/login"),
+                                        new AntPathRequestMatcher("/app/kakao"),
+                                        new AntPathRequestMatcher("/app/kakao/**"),
+                                        new AntPathRequestMatcher("/app/habit/**"),
+                                        new AntPathRequestMatcher("/error")
+                                ).permitAll()
+                                .anyRequest().permitAll())
+                .csrf(AbstractHttpConfigurer::disable)
+                        .formLogin(AbstractHttpConfigurer::disable);
 
         return httpSecurity.build();
     }
+
+
 }
